96e550a8240dba19e1cdcfe63a7ea00fe7faf9a0
Angga rizki <radmvarc@gmail.com>
2024-03-29 16:21:42 +0700

    4# Login User API

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,8 +1,10 @@
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 import { prismaClient } from "../application/database";
 import { ResponseError } from "../error/response-error";
 import {
   CreateUserRequest,
+  LoginUserRequest,
   UserResponse,
   toUserResponse,
 } from "../model/user-model";
@@ -38,5 +40,46 @@ export class UserService {
 
     // kembalikan response dengan konversi model ke response
     return toUserResponse(user);
+  }
+
+  static async login(request: LoginUserRequest): Promise<UserResponse> {
+    const loginRequest = Validation.validate(UserValidation.LOGIN, request);
+
+    // cari apakah user sudah terdaftar
+    let user = await prismaClient.user.findUnique({
+      where: {
+        username: loginRequest.username,
+      },
+    });
+
+    // jika user tidak ditemukan
+    if (!user) {
+      throw new ResponseError(401, "Username or password is incorrect");
+    }
+
+    // jika user ditemukan, cek password
+    const isPasswordValid = await bcrypt.compare(
+      loginRequest.password,
+      user.password
+    );
+
+    // jika password salah
+    if (!isPasswordValid) {
+      throw new ResponseError(401, "Username or password is incorrect");
+    }
+
+    // jika password benar
+    user = await prismaClient.user.update({
+      where: {
+        username: loginRequest.username,
+      },
+      data: {
+        token: uuid(),
+      },
+    });
+
+    const response = toUserResponse(user);
+    response.token = user.token!;
+    return response;
   }
 }
